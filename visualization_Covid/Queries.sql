7451b21f1caed3be3abe5a63dac1238366b8525e
ilyas <71908714+azertyco@users.noreply.github.com>
2026-08-20 16:34:09 +0200

    Add files via upload

---
+++ visualization_Covid/Queries.sql
@@ -0,0 +1,48 @@
+/*
+================================================================================
+  Queries used for Tableau Project
+================================================================================
+*/
+
+-- 1. Global Numbers
+Select 
+  SUM(new_cases) as total_cases, 
+  SUM(cast(new_deaths as int)) as total_deaths, 
+  SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+where continent is not null 
+order by 1,2;
+
+
+-- 2. Total Death Count per Continent
+Select 
+  location, 
+  SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+  and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc;
+
+
+-- 3. Percent Population Infected per Country
+Select 
+  Location, 
+  Population, 
+  MAX(total_cases) as HighestInfectionCount,  
+  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc;
+
+
+-- 4. Percent Population Infected Progression by Date
+Select 
+  Location, 
+  Population, 
+  date, 
+  MAX(total_cases) as HighestInfectionCount,  
+  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc;
